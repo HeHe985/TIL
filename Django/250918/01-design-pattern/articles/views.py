9375fa9f99b13ac07998ba5cb53424d1bc1a502f
gmlfla0219 <dugmlfla0219@gmail.com>
2025-11-24 16:16:24 +0900

    251124_강의

--- Django/250918/01-design-pattern/articles/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-# articles/ 요청이 들어오면 호출되는 함수
-def index(request):
-    return render(request, 'articles/index.html')
--- Django/250919/django_ws_2_5/libraries/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from libraries import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('libraries/recommend/', views.recommend, name='recommend'),
-    path('libraries/bestseller/', views.bestseller, name='bestseller'),
-]
