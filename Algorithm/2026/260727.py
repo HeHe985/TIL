a820c68bbf11911be8e960357f3051ed83398cfc
여희림 <dugmlfla0219@gmail.com>
2026-07-27 15:27:19 +0900

    Algorithm: 260727

---
+++ Algorithm/2026/260727.py
@@ -0,0 +1,15 @@
+'''
+프로그래머스
+코딩테스트 연습 연습문제 점 찍기
+도움말https://school.programmers.co.kr/learn/courses/30/lessons/140107
+'''
+
+def solution(k, d):
+    
+    answer = 0
+    
+    for a in range(0, d + 1, k):
+        max_b = int(((d - a) * (d + a)) ** 0.5)
+        answer += max_b // k + 1
+    
+    return answer
